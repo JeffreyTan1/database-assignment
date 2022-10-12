7733b27d437de1ddde1971819b18d82d6c5de068
Jeffrey Tan <S3851781@student.rmit.edu.au>
2022-10-12 14:44:20 +1100

    add step form

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,40 @@ import {
   Link,
   VStack,
   Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
+  Flex,
+  Image,
+  HStack,
+  extendTheme,
+} from "@chakra-ui/react";
+import { StepsStyleConfig as Steps } from "chakra-ui-steps";
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import StepFrom from "./StepForm";
 
+
+const theme = extendTheme({
+  components: {
+    Steps,
+  },
+});
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        
-        <VStack spacing={8}>
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+    <Box width={"container.xl"} mx={"auto"} my={"10"}>
+      <Flex
+        width={"full"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        px={2}
+        py={4}
+        borderBottomWidth={1}
+      >
+        <Image src={"/logo.png"} width={85} />
+        <Text fontSize={"2xl"} color={"teal.500"} fontWeight={"semibold"}>
+          Federal Election App
+        </Text>
+        <ColorModeSwitcher />
+      </Flex>
+
+      <StepFrom />
     </Box>
   </ChakraProvider>
-)
+);
