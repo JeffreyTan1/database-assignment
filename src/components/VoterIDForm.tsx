3f28b808abd7449faae04fe09b5d8e1997900253
Jeffrey Tan <S3851781@student.rmit.edu.au>
2022-10-29 22:25:47 +1100

    add additional autocomplete forms

--- src/components/VoterIDForm.tsx
+++ src/components/VoterIDForm.tsx
@@ -28,12 +28,24 @@ const VoterIDForm = (props: Props) => {
       voter_f_name: "",
       voter_l_name: "",
       voter_dob: "",
-      voter_r_address: "",
+      autocomplete_address: "",
+      address_line_1: "",
+      address_line_2: "",
+      suburb: "",
+      state: "",
+      postcode: "",
     },
     onSubmit: (values) => {
+      const address_line_1 = values.address_line_1;
+      const address_line_2 = values.address_line_2;
+      const address_combined = address_line_2 ? `${address_line_1} ${address_line_2}` : address_line_1;
+      const residential_address = [address_combined + ",", values.suburb, values.state, values.postcode].filter(Boolean).join(' ');
+
       const transformedValues = {
-        ...values,
-        voter_dob: new Date(values.voter_dob).toLocaleDateString('en-GB'),
+        voter_f_name: values.voter_f_name,
+        voter_l_name: values.voter_l_name,
+        voter_r_address: residential_address,
+        voter_dob: new Date(values.voter_dob).toLocaleDateString("en-GB"),
       };
       props.submitCallback(transformedValues);
     },
@@ -42,42 +54,42 @@ const VoterIDForm = (props: Props) => {
   const loadWidget = () => {
     // @ts-ignore
     const newWidget = new AddressFinder.Widget(
-      document.getElementById("voter_r_address"),
+      document.getElementById("autocomplete_address"),
       "ADDRESSFINDER_DEMO_KEY",
       "AU"
     );
 
     newWidget.on("result:select", function (fullAddress, metaData) {
-      formik.setFieldValue("voter_r_address", fullAddress);
+      formik.setFieldValue("autocomplete_address", fullAddress ? fullAddress : "");
+      formik.setFieldValue("address_line_1", metaData.address_line_1 ? metaData.address_line_1 : "");
+      formik.setFieldValue("address_line_2", metaData.address_line_2 ? metaData.address_line_2 : "");
+      formik.setFieldValue("suburb", metaData.locality_name ? metaData.locality_name : "");
+      formik.setFieldValue("state", metaData.state_territory ? metaData.state_territory : "");
+      formik.setFieldValue("postcode", metaData.postcode ? metaData.postcode : "");
     });
 
     setWidget(newWidget);
   };
 
   useEffect(() => {
-    // check if dom has a script with id addressfinder-script
-    try {
-       const scriptInDOM = document.getElementById("addressfinder-script");
-       if (widget) {
-         widget.destroy();
-       }
-
-       if (scriptInDOM) {
-         scriptInDOM.remove();
-       }
-       const script = document.createElement("script");
-       script.id = "addressfinder-script";
-       script.src = "https://api.addressfinder.io/assets/v3/widget.js";
-       script.async = true;
-       script.onload = loadWidget;
-       document.body.appendChild(script);
-    } catch (error) {
-      console.error(error);
-    }
+    const script = document.createElement("script");
+    script.id = "addressfinder-script";
+    script.src = "https://api.addressfinder.io/assets/v3/widget.js";
+    script.async = true;
+    script.onload = loadWidget;
+    document.body.appendChild(script);
     
     return () => {
       if (widget) {
         widget.destroy();
+      }
+      const scriptInDOM = document.getElementById("addressfinder-script");
+      if (scriptInDOM) {
+        try {
+          scriptInDOM.remove();
+        } catch (error) { 
+          console.error(error);
+        }
       }
     };
   }, []);
@@ -121,17 +133,61 @@ const VoterIDForm = (props: Props) => {
                 onChange={formik.handleChange}
               />
             </FormControl>
-            <FormControl id="voter_r_address" isRequired>
-              <FormLabel>Residential Address</FormLabel>
-              <div>
-                <Input
-                  type="text"
-                  placeholder="Search for your address..."
-                  value={formik.values.voter_r_address}
-                  onChange={formik.handleChange}
-                />
-              </div>
+            <FormControl id="autocomplete_address">
+              <FormLabel>Address Autocomplete</FormLabel>
+              <Input
+                type="text"
+                placeholder="Search for your address..."
+                value={formik.values.autocomplete_address}
+                onChange={formik.handleChange}
+              />
             </FormControl>
+            <FormControl id="address_line_1" isRequired>
+              <FormLabel>Address Line 1</FormLabel>
+              <Input
+                type="text"
+                placeholder="Address Line 1"
+                value={formik.values.address_line_1}
+                onChange={formik.handleChange}
+              />
+            </FormControl>
+            <FormControl id="address_line_2">
+              <FormLabel>Address Line 2</FormLabel>
+              <Input
+                type="text"
+                placeholder="Address Line 2"
+                value={formik.values.address_line_2}
+                onChange={formik.handleChange}
+              />
+            </FormControl>
+            <FormControl id="suburb" isRequired>
+              <FormLabel>Suburb</FormLabel>
+              <Input
+                type="text"
+                placeholder="Suburb"
+                value={formik.values.suburb}
+                onChange={formik.handleChange}
+              />
+            </FormControl>
+            <FormControl id="state" isRequired>
+              <FormLabel>State</FormLabel>
+              <Input
+                type="text"
+                placeholder="State"
+                value={formik.values.state}
+                onChange={formik.handleChange}
+              />
+            </FormControl>
+            <FormControl id="postcode" isRequired>
+              <FormLabel>Postcode</FormLabel>
+              <Input
+                type="text"
+                placeholder="Postcode"
+                value={formik.values.postcode}
+                onChange={formik.handleChange}
+              />
+            </FormControl>
+            
             <Button type="submit" colorScheme="teal" width="full">
               Next
             </Button>
